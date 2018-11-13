3aca70d95b7eb6accc326a6ffd99bca2e2411c50
Colleen O'Rourke <colleen@sentry.io>
2018-11-13 14:41:51 -0800

    workk plz

---
+++ example.py
@@ -0,0 +1,13 @@
+import sentry_sdk
+from sentry_sdk import capture_exception
+import os
+
+sentry_sdk.init(
+    dsn=os.environ.get('SENTRY_DSN'),
+    release=os.environ.get('COMMIT')
+)
+
+try:
+    im_gonna_break_more()
+except Exception as e:
+    capture_exception(e)
