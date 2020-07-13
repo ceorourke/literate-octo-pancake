a500d989a39f05af94d1d6fdef35aa132d81da6e
Colleen O'Rourke <colleen@sentry.io>
2020-07-13 15:54:49 -0700

    testing

--- example.py
+++ example.py
@@ -3,13 +3,12 @@ from sentry_sdk import capture_exception
 import os
 
 sentry_sdk.init(
-    dsn=os.environ.get('SENTRY_DSN'),
+    dsn='https://c0b1efa652d144108fb523f9e0ce3210@meowlificent.ngrok.io/11',
+    debug=True,
     release=os.environ.get('COMMIT')
 )
 
 try:
-    im_gonna_break_more_3()
+    im_gonna_break()
 except Exception as e:
     capture_exception(e)
-
-# blah 
