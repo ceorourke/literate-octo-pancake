47f05dc1c9b1a798da350639cc255bfd0de1bbf4
Colleen O'Rourke <colleen@sentry.io>
2020-07-13 15:59:57 -0700

    1+1

--- example.py
+++ example.py
@@ -4,11 +4,12 @@ import os
 
 sentry_sdk.init(
     dsn='https://c0b1efa652d144108fb523f9e0ce3210@meowlificent.ngrok.io/11',
-    debug=True,
-    release=os.environ.get('COMMIT')
+    # debug=True,
+    release=os.environ.get('VERSION')
 )
 
 try:
     im_gonna_break()
+    boop = 1 + 1
 except Exception as e:
     capture_exception(e)
